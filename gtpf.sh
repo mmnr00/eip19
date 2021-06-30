813e9ed5f95ad31d04310629e5dd11e2fe75cbcb
mmnr00 <mmnr00@gmail.com>
2021-06-30 16:55:14 +0800

    anis

--- gtpf.sh
+++ gtpf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-git checkout dev_an1
+git checkout dev_an2
 git add -A
 git commit -m "anis"
 git push
 
 git checkout ostg_an
-git merge dev_an1
+git merge dev_an2
 git push
 
 git checkout prod_an
@@ -13,5 +13,5 @@ git merge ostg_an
 git push
 
 git diff HEAD ostg_an
-git diff HEAD dev_an1
-git checkout dev_an1
+git diff HEAD dev_an2
+git checkout dev_an2
