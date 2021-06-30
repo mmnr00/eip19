5529e0c662fa7a59605b90d58b718dc872fd9fd5
mmnr00 <mmnr00@gmail.com>
2021-06-30 17:26:40 +0800

    anis

--- config/routes.rb
+++ config/routes.rb
@@ -139,7 +139,7 @@ Rails.application.routes.draw do
 
 
   # For dtails on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   #WELCOME
+   #WELCOME#
   get '/bantuanOKU', to: 'welcomes#bantuanoku'
   get '/intervensianis', to: 'welcomes#intervensianis'
   get '/saringananis', to: 'welcomes#saringananis'
