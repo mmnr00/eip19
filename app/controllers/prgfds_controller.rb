87eb98383fc580573b060ce1946a5c34bdd77d71
mmnr00 <mmnr00@gmail.com>
2021-06-30 17:39:30 +0800

    anis

--- app/controllers/prgfds_controller.rb
+++ app/controllers/prgfds_controller.rb
@@ -1,13 +1,13 @@
 class PrgfdsController < ApplicationController
 
 	private
-		def prgfds_params
-			params.require(:prgfd).permit(:rate, 
-																		:good, 
-																		:bad, 
-																		:perse_id,
-																		:proge_id)
-		end
+	
+	def prgfds_params
+		params.require(:prgfd).permit(:rate, 
+																	:good, 
+																	:bad, 
+																	:perse_id,
+																	:proge_id)
 	end
 
 end
