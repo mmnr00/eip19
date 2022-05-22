6a10af18c4e24859411bb5ab2cb49b928f335f3e
mmnr00 <mmnr00@gmail.com>
2022-05-22 20:55:55 +0800

    anis

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LdpwwwgAAAAAHxbU_HjnaigxbRhz7Eq40EQEtjP'
-  config.secret_key = '6LdpwwwgAAAAAOcUaaEDC5cgLSf1a8iRqHfiOEre'
+  config.site_key  = ENV['CAPTCHA_SITEKEY']
+  config.secret_key = ENV['CAPTCHA_SECRETKEY']
 end
