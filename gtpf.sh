ece7c7354dc6cc96c4b66bb6f0cd07ca6dc5fb46
mmnr00 <mmnr00@gmail.com>
2023-09-06 20:08:30 +0800

    anis

--- gtpf.sh
+++ gtpf.sh
@@ -4,14 +4,14 @@ git add -A
 git commit -m "anis"
 git push
 
-git checkout ostg-an
+git checkout ostg-1an
 git merge dev-an
 git push
 
-git checkout prod-an
-git merge ostg-an 
+git checkout prod-1an
+git merge ostg-1an 
 git push
 
-git diff HEAD ostg-an
+git diff HEAD ostg-1an
 git diff HEAD dev-an
 git checkout dev-an
