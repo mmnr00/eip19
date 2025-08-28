e9c7140d2dcffafa54eab98c2c70217e2f0054c6
mmnr00 <mmnr00@gmail.com>
2025-08-28 20:19:45 +0800

    anis

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -215,7 +215,7 @@ class WelcomesController < ApplicationController
 			flash[:success] = "Pertanyaan anda telah direkodkan. Pihak kami akan menjawab secepat mungkin. Terima kasih"
 			pars = params[:ans]
 			subject = pars[:subject]
-			to = "admin@yanis.org.my"
+			to = "korporat@yanis.org.my"
 			cc = pars[:email]
 			body = "
 				<b>Nama: </b> #{pars[:name]}<br>
